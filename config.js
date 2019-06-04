0b623c96c16c6bdcfa4add4b98d7b610ac2d9417
Warzecha <damian.warzecha1@gmail.com>
2019-06-04 13:47:46 +0200

    self explainatory config

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 const MAX_DIST = 100;
-
-const STATIC_NODE_COUNT = 60;
-const MOBILE_NODE_COUNT = 3;
-
-const PIXELS_PER_METER = 20;
-
 const WIDTH = 80;
 const HEIGHT = 40;
+const PIXELS_PER_METER = 20;
 
-const WAVE_ANGULAR_RES = 60;
+const WAVE_ANGULAR_RES = 20;
 
+// PARAMETERS TO CHANGE:
 const RELAY_RATIO = 0.3;
-
+const STATIC_NODE_COUNT = 60;
+const MOBILE_NODE_COUNT = 3;
 const TTL = 10;
